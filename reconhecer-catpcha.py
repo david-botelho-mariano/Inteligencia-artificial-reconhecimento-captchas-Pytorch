6c2727bf921a8ba97238348c05f1cdf0d501f1a4
david-botelho-mariano <48680041+david-botelho-mariano@users.noreply.github.com>
2020-04-08 18:46:46 -0300

    v0.3

--- reconhecer-catpcha.py
+++ reconhecer-catpcha.py
@@ -1,12 +1,23 @@
 import torch
 from torchvision import datasets, models, transforms
 import shutil
-import matplotlib.pyplot as plt
-import numpy as np
+from IPython.display import display
+import os
 
-def visualize_model(model_treinado, id_imagem):
-    shutil.copyfile(str(id_imagem) + ".jpg", "dataset/outros/" + str(id_imagem) + ".jpg")
-    shutil.copyfile(str(id_imagem) + ".jpg", "dataset/fone-de-ouvido/" + str(id_imagem) + ".jpg")
+def ler_diretorio_imagens(path_captchas):
+    lista_arquivos_desejados = []
+ 
+    for lista_nome_diretorios, lista_nome_pastas, lista_nome_arquivos in os.walk(path_captchas):
+        for arquivo in lista_nome_arquivos:
+            lista_arquivos_desejados.append(os.path.join(lista_nome_diretorios, arquivo))  
+    
+    return lista_arquivos_desejados
+
+def visualize_model(captcha_path, model_treinado):
+    captcha_path_array = captcha_path.split("/")
+
+    shutil.copyfile(captcha_path, "pytorch-config/outros/" + captcha_path_array[1])
+    shutil.copyfile(captcha_path, "pytorch-config/fone-de-ouvido/" + captcha_path_array[1])
 
     data_transforms = transforms.Compose([
         transforms.Resize(256),
@@ -14,7 +25,7 @@ def visualize_model(model_treinado, id_imagem):
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-    image_datasets = datasets.ImageFolder("dataset/", data_transforms)
+    image_datasets = datasets.ImageFolder("pytorch-config/", data_transforms)
     dataloaders = torch.utils.data.DataLoader(image_datasets, batch_size=4, shuffle=True, num_workers=4)
     dataset_sizes = len(image_datasets)
     class_names = image_datasets.classes[0], image_datasets.classes[1]
@@ -29,32 +40,24 @@ def visualize_model(model_treinado, id_imagem):
             _, preds = torch.max(outputs, 1)
 
             for j in range(inputs.size()[0]):
-                imshow(inputs.cpu().data[j])
-                shutil.os.remove("dataset/fone-de-ouvido/" + str(id_imagem) + ".jpg")
-                shutil.os.remove("dataset/outros/" + str(id_imagem) + ".jpg")
+                shutil.os.remove("pytorch-config/fone-de-ouvido/" + captcha_path_array[1])
+                shutil.os.remove("pytorch-config/outros/" + captcha_path_array[1])
                 return class_names[preds[j]]
 
-def comecar_reconhecimento(model_treinado):
-  for id_imagem in range(1,6,1):
-    resultado = visualize_model(model_treinado, id_imagem)
+def comecar_reconhecimento(captcha_path, model_treinado):
+    resultado = visualize_model(captcha_path, model_treinado)
+
     if resultado == "fone-de-ouvido":
-        print("[ATENCAO] " + str(id_imagem) + ".jpg, eh: " + resultado)
+        print("[ATENCAO] O arquivo: " + captcha_path + ", eh classificado como: " + resultado)
     else:
-        print("[INFO] " + str(id_imagem) + ".jpg, eh: " + resultado)
+        print("[INFO] O arquivo: " + captcha_path + ", eh classificado como: " + resultado)
 
-def imshow(inp, title=None):
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)
+if __name__ == '__main__':
+    print("[+] inteligencia artificial treinada apenas para encontrar fone de ouvido em uma imagem.")
+    
+    device = torch.device("cpu")
+    model_treinado = torch.load('model-fones.key', map_location='cpu')
+    lista_captchas_absolute_path = ler_diretorios_e_subdiretorios("captchas")
 
-device = torch.device("cpu")
-model_treinado = torch.load('model-fones.key', map_location='cpu')
-
-print("[INFO] inteligencia artificial treinada apenas para encontrar fone de ouvido em uma imagem")
-comecar_reconhecimento(model_treinado)
+    for captcha_path in lista_captchas_absolute_path:
+      comecar_reconhecimento(captcha_path, model_treinado)
